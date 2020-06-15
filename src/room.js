ac98b40736ce8644b37353b8921438081b505370
hassanrehman01398 <hassanrehman01398@gmail.com>
2020-06-15 20:45:12 +0500

    Render states

--- src/room.js
+++ src/room.js
@@ -2,14 +2,38 @@ import React, { useState } from 'react';
 
 
 function Room(){
-    const [islit,outlit]=useState(true);
+    let [islit,outlit]=useState(false);
+    let [age,setage]=useState(20);
+    function updatelit(){
+        outlit(!islit);
+
+
+    }
+    function updateage(){
+        setage(age++);
+
+
+    }
  //console.log(state);
         return(
 
             <div>
 
 
-                My Room : lit {islit} outlit:{outlit}
+                My Room : {islit?'Lit':'dark'};
+                <br/>
+                <br/>
+
+                <button id="button1" onClick={updatelit}>Toogle Light</button>
+                <br/>
+                <br/>
+                
+                My Age : {age};
+              
+                <br/>
+                <br/>
+                
+                <button id="button2" onClick={updateage}>Increase Age</button>
             </div>
         );
 
